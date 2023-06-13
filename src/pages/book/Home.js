09f102402f6faa5fdcb043a166e0ec0f767e5815
rkdwlss2 <rkdwlss2@gmail.com>
2023-06-13 15:22:37 +0900

    changed

---
+++ src/pages/book/Home.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import BookItem from '../../components/BookItem';
+
+const Home = () => {
+  const [books, setBooks] = useState([]);
+
+  // 함수 실행시 최초 한번 실행되는것
+  useEffect(() => {
+    fetch('http://localhost:8080/book')
+      .then((res) => res.json())
+      .then((res) => {
+        // console.log(1, res);
+        setBooks(res);
+      }); // 비동기 함수
+  }, []);
+
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} book={book} />
+      ))}
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/book/UpdateForm.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+
+const UpdateForm = (props) => {
+  const id = props.match.params.id;
+
+  const [book, setBook] = useState({
+    title: '',
+    author: '',
+  });
+
+  useEffect(() => {
+    fetch('http://localhost:8080/book/' + id)
+      .then((res) => res.json())
+      .then((res) => {
+        setBook(res);
+      });
+  }, []);
+
+  const changeValue = (e) => {
+    setBook({
+      ...book,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submitBook = (e) => {
+    e.preventDefault(); // submit이 action을 안타고 자기 할일을 그만함.
+    fetch('http://localhost:8080/book/' + id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      body: JSON.stringify(book),
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          return null;
+        }
+      })
+      .then((res) => {
+        // catch는 여기서 오류가 나야 실행됨.
+        if (res !== null) {
+          props.history.push('/book/' + id);
+        } else {
+          alert('책 수정에 실패하였습니다.');
+        }
+      });
+  };
+
+  return (
+    <Form onSubmit={submitBook}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter Title"
+          onChange={changeValue}
+          name="title"
+          value={book.title}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Author</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter Author"
+          onChange={changeValue}
+          name="author"
+          value={book.author}
+        />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
+
+export default UpdateForm;
